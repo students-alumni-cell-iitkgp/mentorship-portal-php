12f61a131866ca6803c808efff77df5e65c99011
punit121 <punit.kumar1661@gmail.com>
2016-01-26 14:33:34 +0530

    initial commit

---
+++ application/views/welcome_message.php
@@ -0,0 +1,25 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Welcome to Mentorship-Portal</title>
+	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+	<!-- Generic page styles -->
+	<link rel="stylesheet" href="css/style.css">
+	<style type="text/css">
+
+	::selection { background-color: #E13300; color: white; }
+	::-moz-selection { background-color: #E13300; color: white; }
+
+	
+	</style>
+</head>
+<body>
+	<div>
+	<h2 style="text-align:center">Welcome To Mentorship Portal  </h2>
+	</div>
+
+</body>
+</html>
