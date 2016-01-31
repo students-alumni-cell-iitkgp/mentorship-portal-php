cb743cb9855e45dc95470f51f1bb8885717c714b
Utsav Agarwal <uagarwal744@gmail.com>
2016-01-31 16:26:55 +0530

    changes

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -22,41 +22,37 @@ class Welcome extends CI_Controller {
 	{
 		$this->load->view('welcome_message');	
 	}
+	
 	public function users() {
 		$this->load->database();
 		$this->load->model('insert_model');
-		$this->insert_model->form_insert($_POST);
-	}
-	public function users() {
-		$this->load->database();
-		$this->load->model('insert_model');
-			$data = array(
-		'name' => $this->input->post('name'),
-		'email' => $this->input->post('email'),
-		'password' => $this->input->post('password'),
-		'department' => $this->input->post('department'),
-		'hall' => $this->input->post('hall'),
-		'roll' => $this->input->post('roll'),
-		'cgpa' => $this->input->post('cgpa'),
-		'current_acad_year' => $this->input->post('current'),
-		'join_year' => $this->input->post('join'),
-		'passout_year' => $this->input->post('pout'),
-		'no_of_mentees' => $this->input->post('mentee') 
-		);
+		$data = array(
+			'name' => $this->input->post('name'),
+			'email' => $this->input->post('email'),
+			'password' => $this->input->post('password'),
+			'department' => $this->input->post('department'),
+			'hall' => $this->input->post('hall'),
+			'roll' => $this->input->post('roll'),
+			'cgpa' => $this->input->post('cgpa'),
+			'current_acad_year' => $this->input->post('current'),
+			'join_year' => $this->input->post('join'),
+			'passout_year' => $this->input->post('pout'),
+			'no_of_mentees' => $this->input->post('mentee') 
+			);
 		$data1 = array(
-				'phone' => $this->input->post('phone'),
-				'firm' => $this->input->post('firm'),
-				'designation' => $this->input->post('des'),
-				'field_of_work' => $this->input->post('work'),
-				'email' => $this->input->post('email') );
+			'phone' => $this->input->post('phone'),
+			'firm' => $this->input->post('firm'),
+			'designation' => $this->input->post('des'),
+			'field_of_work' => $this->input->post('work'),
+			'email' => $this->input->post('email') );
 		$data2 = array(
-				'pref1' => $this->input->post('pref1'),
-				'pref2' => $this->input->post('pref2'),
-				'pref3' => $this->input->post('pref3'),
-				'email' => $this->input->post('email') );
+			'pref1' => $this->input->post('pref1'),
+			'pref2' => $this->input->post('pref2'),
+			'pref3' => $this->input->post('pref3'),
+			'email' => $this->input->post('email') );
 
 
-			
+		
 
 
 
@@ -69,5 +65,5 @@ class Welcome extends CI_Controller {
 		$this->load->view('dashboard', $_POST);
 
 	}
- 
+	
 }
