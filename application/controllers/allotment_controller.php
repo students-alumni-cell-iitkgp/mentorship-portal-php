16bf5373cd13ef47284337edf67b50323b54b062
punit121 <punit.kumar1661@gmail.com>
2016-03-18 23:07:11 +0530

    allotment-initiated

--- application/controllers/allotment_controller.php
+++ application/controllers/allotment_controller.php
@@ -10,20 +10,30 @@ class Allotment_controller extends CI_Controller
 	public function allot()
 	{
 		$a= array();
-		$b= array();
-		$c= array();
+		$mentor= array();
+		$mentee= array();
 		$this->load->database();
 		$query = $this->db->query('SELECT * FROM preferences ');
 		$row=$query->row_array();
 		$a = $row;
-		$query = $this->db->query('SELECT * FROM users INNER JOIN preferences ON users.email=preferences.email WHERE cgpa !="" ORDER BY current_acad_year DESC,cgpa DESC  ');
+		$query = $this->db->query('SELECT * FROM users INNER JOIN preferences ON users.email=preferences.email WHERE cgpa !="" ORDER BY current_acad_year DESC,cgpa DESC  '); //mentee
 		$row=$query->row_array();
-		$b = $row;
-		$query = $this->db->query('SELECT * FROM users INNER JOIN preferences ON users.email=preferences.email WHERE passout_year!="" ORDER BY passout_year DESC ');
+		$mentee = $row;
+		$query = $this->db->query('SELECT * FROM users INNER JOIN preferences ON users.email=preferences.email WHERE passout_year!="" ORDER BY passout_year ASC '); //mentor
 		$row=$query->row_array();
-		$c = $row;
-
+		$mentor = $row;
+		$this->match($mentee,$mentor);
  
+
+
+	}
+	public function match($mentee,$mentor)
+	{
+
+		if($mentee['pref1']==$mentor['pref1'])
+		{
+
+		}
 
 
 	}
